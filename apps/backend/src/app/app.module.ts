ab34b12b78241e63f1a3ae4fbdc31a3dc4b6dca0
Juan <34877088+rengifero@users.noreply.github.com>
2026-02-11 12:58:01 -0600

    correccion en global env

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ProductsModule } from './porducts/products.module';
+import { PrismaModule } from '../prisma/prisma.module';
+
 
 @Module({
   imports: [
+      // 1. Configurar variables de entorno globalmente
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'backend/dist/schema.gql'),
+        
       sortSchema: true,
        introspection: true,  
     }),
     ProductsModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],

3b5d517e91d1f6a1f94258fb9e0d8ac4ab43b9cb
Juan <34877088+rengifero@users.noreply.github.com>
2026-02-11 23:50:04 -0600

    add cors on main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,6 +9,15 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure CORS, specifying allowed headers
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your client's origin (e.g., your frontend URL)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, X-Apollo-Operation-Name, Apollo-Require-Preflight', // Include necessary headers
+    credentials: true, // If you need to handle cookies/credentials
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;

2081ba5a18f1d64db23a2e92f4df9e610d066895
Juan <34877088+rengifero@users.noreply.github.com>
2026-03-11 10:29:35 -0600

    TESTING EN BACKEND

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello mundo cruel API' };
+    return { message: 'Hello mundo CREUEL API' };
   }
 }
